86e243fdada6e8d82b2239ea8d8c4d1bfa5da3b4
renxi <retixi@163.com>
2016-11-18 11:51:35 +0800

    多重sql与图形化静态展示成功

--- routes/chart.js
+++ routes/chart.js
@@ -4,11 +4,23 @@ var router = express.Router();
 var sqlite3 = require('sqlite3');
 var db = new sqlite3.Database('sqldb');
 
-
-db.all("SELECT pubdate,COUNT(count)as count FROM saleslist group by pubdate",function (err,rows) {
+var countsum_by_date="SELECT pubdate,sum(count)as count FROM saleslist group by pubdate order by pubdate"
+var pricesum_by_name="SELECT name,sum(price) as price FROM saleslist group by name"
+var pricesum_by_area="SELECT area,sum(price) as price FROM saleslist group by area"
+var pricesum_by_cate="SELECT cate,sum(price) as price FROM saleslist group by cate"
+db.all(countsum_by_date,function (err,rows1) {
+db.all(pricesum_by_name,function (err,rows2) {
+db.all(pricesum_by_area,function (err,rows3) {
+db.all(pricesum_by_cate,function (err,rows4) {
     router.get('/',function (req,res) {
-        console.log(rows)
-        res.render('chart.html',{datas:rows
+        res.render('chart.html',{countsum_by_date:rows1,
+                                pricesum_by_name:rows2,
+                                pricesum_by_area:rows3,
+                                pricesum_by_cate:rows4})
+    })
+    console.log(rows2);
+    console.log(rows3);
+            })
         })
     })
 })
